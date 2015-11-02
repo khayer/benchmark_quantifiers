f21f91d3a1c83612102d38ca33430ef3f9f41da2
Katharina Hayer <katharinaehayer@gmail.com>
2015-11-01 19:33:42 -0500

    hitting the pause button

--- lib/benchmark_quantifiers/rnaseq_functions.rb
+++ lib/benchmark_quantifiers/rnaseq_functions.rb
@@ -1,6 +1,38 @@
 module RNAseqFunctions
   # FLD = fragment length distribution
   def RNAseqFunctions.effective_length(transcript_length, mean_FLD)
-    transcript_length - mean_FLD + 1
+    transcript_length - mean_FLD.to_f + 1.0
   end
+
+  def RNAseqFunctions.effective_count(raw_count, transcript_length, effective_length)
+    (raw_count * (transcript_length / effective_length.to_f)).round(4)
+  end
+
+  def RNAseqFunctions.counts_per_million(raw_count, number_of_assigned_reads)
+    ((raw_count.to_f / number_of_assigned_reads.to_f) * (10.0 ** 6)).round(4)
+  end
+
+  # Transcripts per million (TPM)
+  #def RNAseqFunctions.tpm(raw_count, effective_length, sum_over_all_transcripts)
+  #  x = 1 / (sum_over_all_transcripts)
+  #  ((raw_count.to_f /effective_length.to_f) * x * (10.0 ** 6)).round(4)
+  #end
+
+  #def RNAseqFunctions.sum_over_all_transcripts()
+  #  #TODO
+  #  return
+  #end
+
+  # Fragments per kilobase of exon per million reads mapped
+  def RNAseqFunctions.fpkm(raw_count, effective_length, number_of_assigned_reads)
+    (raw_count.to_f / number_of_assigned_reads.to_f * effective_length * (10.0 ** 9)).round(4)
+  end
+
+  def RNAseqFunctions.tpm_from_fpkm(fpkm_current, fpkm_sum_over_all_transcripts)
+    ((fpkm_current.to_f / fpkm_sum_over_all_transcripts.to_f) * (10.0 ** 6)).round(4)
+  end
+
+  #def RNAseqFunctions.fpkm_sum_over_all_transcripts()
+  #  #TODO
+  #end
 end
--- test/test_rnaseq_functions.rb
+++ test/test_rnaseq_functions.rb
@@ -2,12 +2,29 @@ require 'minitest_helper'
 
 class TestRNAseqFunctions < Minitest::Test
   def setup
-
+    @effective_length = RNAseqFunctions.effective_length(2928,213.96)
+    @number_of_assigned_reads = 80*10**6
   end
 
   def test_effective_length
     l = RNAseqFunctions.effective_length(43553,213.96)
     assert_equal(43340.04, l)
+  end
+
+  def test_effective_count
+    l = RNAseqFunctions.effective_count(874, 2928, @effective_length)
+    assert_equal(942.5541, l)
+  end
+
+  def test_counts_per_million
+    l = RNAseqFunctions.counts_per_million(70000, @number_of_assigned_reads)
+    assert_equal(875.0, l)
+  end
+
+
+  def test_fpkm
+    l = RNAseqFunctions.fpkm(80, @effective_length, @number_of_assigned_reads)
+    assert_equal(2715040.0, l)
   end
 
   def test_that_will_be_skipped
