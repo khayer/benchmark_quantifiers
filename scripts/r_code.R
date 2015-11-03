2f4292ecab0d2a75d67ff5001c0d9f98520c5bf3
Katharina Hayer <katharinaehayer@gmail.com>
2015-11-03 09:36:44 -0500

    hitting the pause button

---
+++ scripts/r_code.R
@@ -0,0 +1,20 @@
+# Test R^2
+
+all_trans_estimates = c(103.5,22.4,43.5,102.0,43.0,200.0)
+all_truth_estimates = c(103.7,12.4,43.5,103.0,19.0,150.0)
+
+plot(all_truth_estimates,all_trans_estimates)
+cor(all_truth_estimates,all_trans_estimates)
+
+fit <- lm(all_truth_estimates ~ all_trans_estimates)
+fit
+k = summary(fit, correlation = TRUE, method="pearson")
+k$r.squared
+
+(k$r.squared)^2
+
+library(Hmisc)
+rcorr(all_truth_estimates,all_trans_estimates, type="pearson")
+
+
+RMSE <- sqrt(mean((all_trans_estimates-all_truth_estimates)^2))
--- test/test_functions.rb
+++ test/test_functions.rb
@@ -6,24 +6,24 @@ class TestRNAseqFunctions < Minitest::Test
   end
 
   def test_root_mean_squared_error
-    all_trans_estimates = [103.5,22.4,43.5,10002.0]
-    all_truth_estimates = [103.7,12.4,43.5,10003.0]
+    all_trans_estimates = [103.5,22.4,43.5,102.0,43.0,200.0]
+    all_truth_estimates = [103.7,12.4,43.5,103.0,19.0,150.0]
     l = Functions.root_mean_squared_error(all_trans_estimates, all_truth_estimates)
-    assert_equal(5.0259, l)
+    assert_equal(23.011, l)
   end
 
   def test_pearson_correlation_coefficient
-    all_trans_estimates = [103.5,22.4,43.5,10002.0,43.0,200.0]
-    all_truth_estimates = [103.7,12.4,43.5,10003.0,0.0,0.0]
+    all_trans_estimates = [103.5,22.4,43.5,102.0,43.0,200.0]
+    all_truth_estimates = [103.7,12.4,43.5,103.0,19.0,150.0]
     l = Functions.pearson_correlation_coefficient(all_trans_estimates, all_truth_estimates)
-    assert_equal(0.9998, l)
+    assert_equal(0.9574, l)
   end
 
   def test_squared_pearson_correlation_coefficient
-    all_trans_estimates = [103.5,22.4,43.5,10002.0,43.0,200.0]
-    all_truth_estimates = [103.7,12.4,43.5,10003.0,0.0,0.0]
+    all_trans_estimates = [103.5,22.4,43.5,102.0,43.0,200.0]
+    all_truth_estimates = [103.7,12.4,43.5,103.0,19.0,150.0]
     l = Functions.squared_pearson_correlation_coefficient(all_trans_estimates, all_truth_estimates)
-    assert_equal(0.9996, l)
+    assert_equal(0.9166, l)
   end
 
 end
