ee7c5d15e83a6206a983553be654e85e9a9681d2
Katharina Hayer <katharinaehayer@gmail.com>
2015-11-09 17:16:52 -0500

    hitting the pause button

--- bin/calculate_metrics.rb
+++ bin/calculate_metrics.rb
@@ -4,9 +4,15 @@ require "benchmark_quantifiers"
 # TranscriptLengths  TranscriptCounts mean_FLD
 # outfile:
 # FPKM TPM etc.
-
-mean_FLD = 269.61
-std_FLD = 39.84
+#LONG
+#mean_FLD = 269.61
+#std_FLD = 39.84
+# MEDIUM
+#mean_FLD = 269.61
+#std_FLD = 39.84
+# SHORT
+mean_FLD = 260.0
+std_FLD = 44.0
 gene_to_ens = {}
 ensg_to_enstrans = {}
 ens_lengths = {}
@@ -22,7 +28,8 @@ File.open("files/transcript_length.txt").each do |line|
 end
 
 sum_all_counts = 0
-File.open("files/ENS.PLD.counts_for_FPKM.txt").each do |line|
+#File.open("files/ENS.PLD.counts_for_FPKM.txt").each do |line|
+File.open("files/ENS.PSD.counts_for_FPKM.txt").each do |line|
   line.chomp!
   next if line =~ /^gene/
   gene_id, cnt = line.split("\t")
@@ -35,7 +42,8 @@ end
 fpkm_per_transcript = {}
 effective_length_per_transcript = {}
 fpkm_sum_over_all_transcripts = 0
-File.open("files/ENS.PLD.counts_for_FPKM.txt").each do |line|
+#File.open("files/ENS.PLD.counts_for_FPKM.txt").each do |line|
+File.open("files/ENS.PSD.counts_for_FPKM.txt").each do |line|
   line.chomp!
   next if line =~ /^gene/
   gene_id, cnt = line.split("\t")
@@ -47,13 +55,19 @@ File.open("files/ENS.PLD.counts_for_FPKM.txt").each do |line|
   if effective_length < 0
     effective_length += std_FLD
   end
-  if effective_length < 0
+  if effective_length <= 0
     effective_length = 1
   end
   effective_length_per_transcript[gene_to_ens[gene_id]] = effective_length
   #puts effective_length
   #exit
   k = RNAseqFunctions.fpkm(cnt.to_i/2, effective_length, sum_all_counts)
+  #if k.infinite?
+  #  puts cnt.to_i/2
+  #  puts effective_length
+  #  puts sum_all_counts
+  #  exit
+  #end
   fpkm_per_transcript[gene_to_ens[gene_id]] = k
   fpkm_sum_over_all_transcripts += k
   #puts fpkm_per_transcript
@@ -61,17 +75,18 @@ File.open("files/ENS.PLD.counts_for_FPKM.txt").each do |line|
 end
 
 puts fpkm_per_transcript
+puts fpkm_sum_over_all_transcripts
 
 tpm_per_transcript = {}
 fpkm_per_transcript.each_pair do |key,value|
-
   k = RNAseqFunctions.tpm_from_fpkm(value, fpkm_sum_over_all_transcripts)
   tpm_per_transcript[key] = k
 end
 
 puts tpm_per_transcript
 
-out_file = File.open("files/transcript_metrics_ENS.PLD.txt", "w")
+#out_file = File.open("files/transcript_metrics_ENS.PLD.txt", "w")
+out_file = File.open("files/transcript_metrics_ENS.PSD.txt", "w")
 out_file.puts "GENEID\ttrans_id\tgene_id\tlength\t#exons\teffective_length\tFPKM\tTPM"
 File.open("files/transcript_length.txt").each do |line|
   line.chomp!
